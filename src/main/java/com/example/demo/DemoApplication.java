4086b6317eae351be8e55a21956410e89eab71ad
akshatsharma1205 <akshatsharma1205@gmail.com>
2019-05-29 11:36:34 +0530

    sent otp

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 
 @SpringBootApplication
 @RestController
@@ -16,5 +18,18 @@ public class DemoApplication {
 	@GetMapping("/sendotp")
 	public String hello() {
 		return "hello world!";
+	}
+	
+	private static final String template = "%s";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/number")
+	public PhoneNumber phoneNumber(@RequestParam(value="name", defaultValue="World") String name) {
+		SendOtp so = new SendOtp("", "+91" + name);
+		so.Send_OTP();
+		return new PhoneNumber(counter.incrementAndGet(),
+				String.format(template, name));
+		
+		
 	}
 }
